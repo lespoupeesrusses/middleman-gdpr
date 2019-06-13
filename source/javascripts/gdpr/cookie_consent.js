a1034b4be70b4d11e891206347d51d7a40bfb339
Arnaud Levy <alevy@lespoupees.paris>
2019-06-13 14:44:06 +0200

    better style

--- source/javascripts/gdpr/cookie_consent.js
+++ source/javascripts/gdpr/cookie_consent.js
@@ -1,20 +1,21 @@
 //= require js-cookie/src/js.cookie
 
 window.addEventListener('load', function() {
-  var gdprCookieConsent = document.querySelector('.js-gdpr__cookie_consent');
+    var cookieKey = 'gdpr.cookie_consent.ok',
+        cookieAlreadySet = Cookies.get(cookieKey) === 'true';
+        gdprCookieConsent = document.querySelector('.js-gdpr__cookie_consent');
 
-  if(!gdprCookieConsent) return;
-  var COOKIE_KEY = 'gdpr.cookie_consent.ok'
+    if(!gdprCookieConsent) {
+        return;
+    }
 
-  var remove = function() {
-    if(gdprCookieConsent.parentNode) gdprCookieConsent.parentNode.removeChild(gdprCookieConsent);
-  }
-
-  if(Cookies.get(COOKIE_KEY) === 'true') remove()
-  else {
-    gdprCookieConsent.querySelector('.js-gdpr__cookie_consent__buttons__ok').addEventListener('click', function() {
-      Cookies.set(COOKIE_KEY, true, {path: '/', expires: 365});
-      remove()
-    });
-  }
+    if (!cookieAlreadySet) {
+        gdprCookieConsent.classList.add('gdpr__cookie_consent--necessary');
+        gdprCookieConsent.querySelector('.js-gdpr__cookie_consent__buttons__ok').addEventListener('click', function() {
+            Cookies.set(cookieKey, true, {path: '/', expires: 365});
+            if (gdprCookieConsent.parentNode) {
+                gdprCookieConsent.parentNode.removeChild(gdprCookieConsent)
+            };
+        });
+    }
 });
